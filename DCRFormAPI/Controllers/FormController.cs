968a15b1daca6c4043783eeb2115f8e3eb335cb3
Michelle Karlsson <michelle@netkarlsson.dk>
2020-12-01 15:39:13 +0100

    FormAPI

---
+++ DCRFormAPI/Controllers/FormController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FormAPI.Controllers
+{
+    public class InitializeFormInput
+    {
+        public string Xml { get; set; }
+    }
+
+    public class ExecuteFieldInput
+    {
+        public int Formid { get; set; }
+        
+        public string Activityid { get; set; }
+        public string Value { get; set; }
+        public string Userid { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class SubmitInput
+    {
+        public int Formid { get; set; }
+    }
+
+    public class GetFormFieldsInput
+    {
+        public int Formid { get; set; }
+    }
+
+    [ApiController]
+    [Route("form")]
+    public class FormController : ControllerBase
+    {
+        [Route("InitialiseForm"), HttpPost]
+        public IActionResult InitialiseForm(InitializeFormInput input)
+        {
+            /*InitializeForm(<dcr xml>) -> formid (integer) – 
+             * save the graph in an array with the formid being the index in the map*/
+            var xml = input.Xml;
+            IActionResult FormId = FormAPI.FormManager.InitialiseForm(xml);
+            return FormId;
+        }
+
+        [Route("GetForm/{id}"), HttpGet]
+        public IActionResult GetForm(int id)
+        {
+            //GetForm(formid) -> form(JSON)
+            return FormAPI.FormManager.GetForm(id);
+        }
+
+        [Route("ExecuteField"), HttpPost]
+        public IActionResult ExecuteField(ExecuteFieldInput input)
+        {
+            //ExecuteField(formid, activity id, value, user id, role) -> void
+            var success = FormAPI.FormManager.ExecuteField(input.Formid, input.Activityid, input.Value, input.Userid, input.Role);
+            if (success != null) { return success; }
+            return success;
+        }
+
+        [Route("GetFormFields/{id}"), HttpGet]
+        public IActionResult GetFormFields(int id)
+        {
+            //GetFormFields(formid) -> form fields (JSON) – like GetEnabledOrPending in a task list
+            return FormAPI.FormManager.GetFormFields(id);
+            
+        }
+
+        [Route("Submit"), HttpPost]
+        public IActionResult Submit(SubmitInput input)
+        {
+            //Submit(formid) -> dcr xml log (xml) – also remove the form id from a list of active form ids
+            return FormAPI.FormManager.Submit(input.Formid);
+        }
+
+        [Route("ExecuteAndFormFields"), HttpPost]
+        public IActionResult ExecuteAndFormFields(ExecuteFieldInput input)
+        {
+            //ExecuteAndFormFields(formid, activity id, value, user id, role)
+            return FormAPI.FormManager.ExecuteAndFormFields(input.Formid, input.Activityid, input.Value, input.Userid, input.Role);
+        }
+    }
+}
---
+++ DCRFormAPI/FormAPI/FormManager.cs
@@ -0,0 +1,99 @@
+﻿using DCR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FormAPI.FormAPI
+{
+    public static class FormManager
+    {
+
+        private static Dictionary<int, Graph> ActiveForms;
+        private static int currentIndex = 0;
+
+        /*Takes a form, adds it to the map of active forms*/
+        public static IActionResult InitialiseForm(string input)
+        {
+            if (ActiveForms == null)
+            {
+                ActiveForms = new Dictionary<int, Graph>();
+            }
+
+            int index = currentIndex++;
+            XDocument xmlInput = XDocument.Parse(input);
+            Graph graph = Graph.FromXml(xmlInput);
+            graph.Initialise();
+            ActiveForms.Add(index, graph);
+            return new OkObjectResult(index);
+        }
+
+        public static IActionResult ExecuteField(int formid, string activityid, string value, string userid, string role)
+        {
+            var success = ActiveForms.TryGetValue(formid, out Graph graph);
+            if (!success) { return new UnprocessableEntityObjectResult(formid + " is not active"); }
+
+            try
+            {
+                Form.execute(graph, new DCR.Path(activityid), Value.Parse(value), userid, role);
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e.InnerException.Message);
+            }
+            return new OkResult();
+        }
+
+        public static IActionResult GetFormFields(int formid)
+        {
+            var success = ActiveForms.TryGetValue(formid, out Graph graph);
+            if (!success) { return new UnprocessableEntityObjectResult(formid + " is not active"); }
+            StringWriter sw = new StringWriter();
+            Form.getFormFields(graph, sw);
+            return new OkObjectResult(sw.ToString());
+        }
+
+        public static IActionResult GetForm(int formid)
+        {
+            var success = ActiveForms.TryGetValue(formid, out Graph graph);
+            if (!success) { return new UnprocessableEntityObjectResult(formid + " is not active"); }
+            StringWriter sw = new StringWriter();
+            Form.getForm(graph, sw);
+            return new OkObjectResult(sw.ToString());
+        }
+
+        public static IActionResult ExecuteAndFormFields(int formid, string activityid, string value, string userid, string role)
+        {
+            var success = ActiveForms.TryGetValue(formid, out Graph graph);
+            if (!success) { return new UnprocessableEntityObjectResult(formid + " is not active"); }
+
+            try
+            {
+                StringWriter sw = new StringWriter();
+                Form.executeAndFormFields(graph, new DCR.Path(activityid), Value.Parse(value), userid, role, sw);
+                return new OkObjectResult(sw.ToString());
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e.InnerException.Message);
+            }
+            
+        }
+
+        public static IActionResult Submit(int formid)
+        {
+            var success = ActiveForms.TryGetValue(formid, out Graph graph);
+            if (!success) { return new UnprocessableEntityObjectResult(formid + " is not active"); }
+
+            XElement xml = Form.submit(graph);
+            ActiveForms.Remove(formid);
+            return new OkObjectResult(xml.ToString());
+
+        }
+
+    }
+}
